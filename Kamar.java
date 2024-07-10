6f7bedc7dc8dd0af7527a4702953276fcd8ee2dc
zan <137709585+rfq-zan@users.noreply.github.com>
2024-07-10 11:50:25 +0700

    Add files via upload

--- Kamar.java
+++ Kamar.java
@@ -8,6 +8,62 @@ package sewakamar;
  *
  * @author ASUS
  */
-public class Kamar {
-    
+// Pembuatan class kamar untuk menyimpan data harga dari biaya per kamar
+abstract class Kamar {
+// Pembuatan access modifier dan tipe data dengan atrribut yang berupa jenis kamar 
+private double Reguler;
+private double DoubleReguler;
+private double Premium;
+private double Deluxe;
+private double SuperPremium;
+
+// Pembuatan constructor guna menyimpan value dari data yang sudah ditetapkan oleh user
+public Kamar(double Reguler, double DoubleReguler, double Premium, double Deluxe, double SuperPremium){
+this.Reguler = Reguler;    
+this.DoubleReguler = DoubleReguler;
+this.Premium = Premium;
+this.Deluxe = Deluxe;
+this.SuperPremium = SuperPremium;
+}
+
+// Setter getter method untuk mengambil data dan menyimpan data apabila nantinya sudah terkena diskon
+    public double getReguler() {
+        return Reguler;
+    }
+
+    public void setReguler(double Reguler) {
+        this.Reguler = 100.000;
+    }
+
+    public double getDoubleReguler() {
+        return DoubleReguler;
+    }
+
+    public void setDoubleReguler(double DoubleReguler) {
+        this.DoubleReguler = 200.000;
+    }
+
+    public double getPremium() {
+        return Premium;
+    }
+
+    public void setPremium(double Premium) {
+        this.Premium = 250.000;
+    }
+
+    public double getDeluxe() {
+        return Deluxe;
+    }
+
+    public void setDeluxe(double Deluxe) {
+        this.Deluxe = 350.000;
+    }
+
+    public double getSuperPremium() {
+        return SuperPremium;
+    }
+
+    public void setSuperPremium(double SuperPremium) {
+        this.SuperPremium = 500.000;
+    }
 }
--- SewaKamar.java
+++ SewaKamar.java
@@ -3,18 +3,93 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package sewakamar;
-
+// Penulisan import agar program dapat berjalan secara dinamis
+import java.util.Scanner;
 /**
  *
  * @author ASUS
  */
-public class SewaKamar {
-
+// main class mengekstensi kelas parent yaitu Kamar.java
+public class SewaKamar extends Kamar{
+ 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+// Penulisan attribute didalam class SewaKamar 
+ int tipeKamar;
+ double hargaKamar;
+ String namaPenyewa;
+//        Pembuatan objek scanner agar program dapat diinputkan secara dinamis
+        Scanner scanner = new Scanner();
+//        Perulangan do while digunakan agar program dapat diulang saat user menginputkan y/n
+        do{
+            System.out.println("==============================");
+            System.out.println("Sewa Kamar");
+            System.out.println("==============================");
+//            Tipe kamar yang disewakan oleh hotel
+            System.out.println("Pilih Tipe Kamar Yang Anda Inginkan!");
+            System.out.println("1. Reguler (Kapasitas : 1 Orang)");
+            System.out.println("   Harga : Rp. 100.000");
+            System.out.println("2. Double Reguler (Kapasitas : 2 Orang)");
+            System.out.println("   Harga : Rp. 200.000");
+            System.out.println("3. Premium (Kapasitas : 2 Orang)");
+            System.out.println("   Harga : Rp. 250.000");
+            System.out.println("4. Deluxe (Kapasitas : 2 Orang)");
+            System.out.println("   Harga : Rp. 350.000");
+            System.out.println("5. Super Premium (Kapasitas : 2 Orang)");
+            System.out.println("   Harga : Rp. 500.000");
+//            User diminta memasukkan pilihan kamar yang akan disewa
+            System.out.println("Masukkan Pilihan anda : ");
+            scanner.next();
+            scanner.nextLine();
+//            Memasukkan kriteria administrasi hotel
+            SewaKamar Nama = new SewaKamar();
+            System.out.println("Masukkan Nama Anda : ");
+            scanner.next(Nama);
+            SewaKamar Umur = new SewaKamar();
+            System.out.println("Masukkan Umur Anda : ");
+            scanner.nextInt(Umur);
+            SewaKamar Hari = new SewaKamar();
+            System.out.println("Sewa Kamar Berapa Lama (Hari) : ");
+            scanner.nextInt(Hari);
+            System.out.println("==============================");
+            
+//            Perulangan if else digunakan untuk menentukan apakah si penginput suddah diatas umur 19 tahun
+            if(Umur >= 19){
+                continue;
+            } else{
+            System.out.println("==============================");
+                System.out.println("Maaf Anda Belum Cukup Umur Untuk Memesan Kamar");
+            System.out.println("==============================");
+                break;
+            }
+        }while();
+        
+//      Switch case digunakan agar program berjalan sesuai keinginan saat diinputkan pemilihan kriteria penyewaan kamar        
+        switch(){
+            case 1 : Reguler(scanner);
+            break;
+            case 2 : DoubleReguler(scanner);
+            break;
+            case 3 : Premium(scanner);
+            break;
+            case 4 : Deluxe(scanner);
+            break;
+            case 5 : SuperPremium(scanner);
+            break;
+                
+        }
+        
+//        pembuatan DisplayData guna mengoutputkan data yang nantinya akan dioutputkan saat user telah memasukkan kriteria program
+        private void DisplayData(){
+            System.out.println("Jenis Kamar : " +);
+            System.out.println("Nama Tamu : " +Nama);
+            System.out.println("Lama Menginap : " +Hari);
+            System.out.println("Total Harga Sebelum Diskon : " +hargaAwal);
+            System.out.println("Diskon : " +disc);
+            System.out.println("Total Harga Setelah Diskon : " +totalidr);
+        }
     }
     
 }
